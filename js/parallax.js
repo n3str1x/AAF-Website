8ab4a18cce6d0838d910ee55f754f6b18d4c61a0
Alex Sinnott <chess2ryme@gmail.com>
2016-01-15 00:30:01 +1100

    Initial Commit

---
+++ js/parallax.js
@@ -0,0 +1,112 @@
+var ParallaxManager, ParallaxPart;
+
+ParallaxPart = (function() {
+  function ParallaxPart(el) {
+    this.el = el;
+    this.speed = parseFloat(this.el.getAttribute('data-parallax-speed'));
+    this.maxScroll = parseInt(this.el.getAttribute('data-max-scroll'));
+    this.maxScroll = parseInt(this.el.getAttribute('data-opacity-start'));
+    this.maxScroll = parseInt(this.el.getAttribute('data-opacity-end'));
+  }
+
+  ParallaxPart.prototype.update = function(scrollY) {
+    if (scrollY > this.maxScroll) { return; }
+    var offset = -(scrollY * this.speed);
+    this.setYTransform(offset);
+  };
+
+  ParallaxPart.prototype.setYTransform = function(val) {
+    this.el.style.webkitTransform = "translate3d(0, " + val + "px, 0)";
+    this.el.style.MozTransform    = "translate3d(0, " + val + "px, 0)";
+    this.el.style.OTransform      = "translate3d(0, " + val + "px, 0)";
+    this.el.style.transform       = "translate3d(0, " + val + "px, 0)";
+    this.el.style.msTransform     = "translateY(" + val + "px)";
+  };
+
+  return ParallaxPart;
+
+})();
+
+ParallaxManager = (function() {
+  ParallaxManager.prototype.parts = [];
+
+  function ParallaxManager(elements) {
+    if (typeof elements === 'array' && elements.length) {
+      this.elements = elements;
+    }
+    if (typeof elements === 'object' && elements.item) {
+      this.elements = Array.prototype.slice.call(elements);
+    } else if (typeof elements === 'string') {
+      this.elements = document.querySelectorAll(elements);
+      if (this.elements.length === 0) {
+        throw new Error("Parallax: No elements found");
+      }
+      this.elements = Array.prototype.slice.call(this.elements);
+    } else {
+      throw new Error("Parallax: Element variable is not a querySelector string, Array, or NodeList");
+    }
+    for (var i in this.elements) {
+      this.parts.push(new ParallaxPart(this.elements[i]));
+    }
+    window.addEventListener("scroll", this.onScroll.bind(this));
+  }
+
+  ParallaxManager.prototype.onScroll = function() {
+    window.requestAnimationFrame(this.scrollHandler.bind(this));
+  };
+
+  ParallaxManager.prototype.scrollHandler = function() {
+    var scrollY = Math.max(window.pageYOffset, 0);
+    for (var i in this.parts) { this.parts[i].update(scrollY); }
+  };
+
+  return ParallaxManager;
+
+})();
+
+new ParallaxManager('.parallax-layer');
+
+
+/* Test for Mouse or Trackpad and aujust scrolling accordingly */
+
+var scrolling = false;
+var oldTime = 0;
+var newTime = 0;
+var isTouchPad;
+var eventCount = 0;
+var eventCountStart;
+
+var mouseHandle = function (evt) {
+    var isTouchPadDefined = isTouchPad || typeof isTouchPad !== "undefined";
+    console.log("isTouchPadDefined = " + isTouchPadDefined);
+    if (!isTouchPadDefined) {
+        if (eventCount === 0) {
+            eventCountStart = new Date().getTime();
+        }
+
+        eventCount++;
+
+        if (new Date().getTime() - eventCountStart > 50) {
+                if (eventCount > 4) {
+                    isTouchPad = true;
+                } else {
+                    isTouchPad = false;
+                }
+            isTouchPadDefined = true;
+        }
+    }
+
+    if (isTouchPadDefined) {
+        // here you can do what you want
+        // i just wanted the direction, for swiping, so i have to prevent
+        // the multiple event calls to trigger multiple unwanted actions (trackpad)
+        if (!evt) evt = event;
+        var direction = (evt.detail<0 || evt.wheelDelta>0) ? 1 : -1;
+        
+        if (!isTouchPad) {
+            $(".parallax-layer").css("transition","transform 400ms ease");
+        }
+    }
+}
+document.addEventListener("mousewheel", mouseHandle, false);
+document.addEventListener("DOMMouseScroll", mouseHandle, false);
