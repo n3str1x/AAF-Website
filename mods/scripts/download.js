b6984cedbef168cd9034563d4d47faf009c6acb1
Alex Sinnott <chess2ryme@gmail.com>
2016-10-09 15:12:03 +1100

    Ok that was a pretty crappy idea.

--- mods/scripts/download.js
+++ mods/scripts/download.js
@@ -1,10 +1,26 @@
-// @codekit-prepend "granim.js";
+/* // @codekit-prepend "granim.js"; */
+
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+};
 
 var IP;
 var connected = false;
 var connectedNo = 1;
 var info;
 var serverInfo;
+var username = getUrlParameter('username');
 
 jQuery.fn.extend({
     statusColour: function (colour) {
