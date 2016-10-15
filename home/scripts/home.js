bc0d4bc10ab91061ba4f59f3db5879c47c380ff3
Alex Sinnott <chess2ryme@gmail.com>
2016-10-15 15:52:42 +1100

    Minor Fixes to Homepage

--- home/scripts/home.js
+++ home/scripts/home.js
@@ -7,19 +7,20 @@ $('.slick-slider').slick({
     adaptiveHeight: false,
     infinite: true,
     swipe: false,
-    autoplay: false,
+    autoplay: true,
     autoplaySpeed: 4000,
     prevArrow: '<button type="button" data-role="none" class="slick-prev" aria-label="Previous" tabindex="0" role="button"></button>',
     nextArrow: '<button type="button" data-role="none" class="slick-next" aria-label="Next" tabindex="0" role="button"></button>',
     responsive: [
     {
-      breakpoint: 1024,
+      breakpoint: 640,
       settings: {
         infinite: true,
         swipe: true
       }
     }]
 });
+
 $(".slick-list").focus();
 
 $('.slick-slider').on('beforeChange', function(event, slick, currentSlide, nextSlide) {
