5f6b8b1563b5151e89fa1ade3d0837938ad0fb0d
Alex <chess2ryme@gmail.com>
2016-12-16 11:51:19 +1100

    Node Changes

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 var static = require('node-static');
+var ssi = require("ssi");
 var file = new static.Server();
+
+var inputDirectory = "/tmp/test";
+var outputDirectory = "/tmp/output";
+var matcher = ".*";
+
+var includes = new ssi(inputDirectory, outputDirectory, matcher, true);
+includes.compile();
+
 require('http').createServer(function(request, response) {
     request.addListener('end', function() {
         file.serve(request, response);
     }).resume();
-}).listen(process.env.PORT || 80);
+}).listen(80);
